de38f56396a2045f87ba144bbdcd5db879a4258f
天海网络 <tianhaiwangluo@tianhaiwangluodeMac-mini.local>
2017-06-14 10:11:08 +0800

    add detailViewController

---
+++ Example/THMobileLibrary/Main/ElectronResource/Controller/THDetailViewController.h
@@ -0,0 +1,20 @@
+//
+//  THDetailViewController.h
+//  THMobileLibrary
+//
+//  Created by 天海网络  on 2017/6/14.
+//  Copyright © 2017年 C-bin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface THDetailViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *bookImage;   //图书图片
+@property (weak, nonatomic) IBOutlet UILabel *bookName;        //书名
+@property (weak, nonatomic) IBOutlet UILabel *author;          //作者
+@property (weak, nonatomic) IBOutlet UILabel *publishingHouse; //出版社
+@property (weak, nonatomic) IBOutlet UILabel *isbn;            //ISBN
+@property (weak, nonatomic) IBOutlet UILabel *fileFormat;      //文件格式
+@property (weak, nonatomic) IBOutlet UILabel *price;           //定价
+
+@end
